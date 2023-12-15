691e43726ee873ea5a7fbae02d885aff1b43d5c9
tyler kim <yoonsoo.kim11@gmail.com>
2023-12-15 16:55:00 -0500

    code along

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,26 @@
 import React, { useState } from "react";
 
 function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+ 
+  const [formData, setFormData] = useState({
+    firstName: "John",
+    lastName: "Henry"
+  })
+  console.log(formData)
 
-  function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
-  }
-
-  function handleLastNameChange(event) {
-    setLastName(event.target.value);
-  }
+ const handleChange = (e) =>{
+  const name = e.target.name;
+  const value = e.target.value;
+    setFormData({
+      ...formData,
+      [name]: value
+    })
+ }
 
   return (
     <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
+      <input type="text" onChange={handleChange} name='firstName' value={formData.firstName} />
+      <input type="text" onChange={handleChange} name='lastName' value={formData.lastName} />
       <button type="submit">Submit</button>
     </form>
   );
